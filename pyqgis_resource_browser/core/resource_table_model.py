c943fda8b5770980e542f84fe8ac8da8a0d3623f
GeoJulien <dev@ingeoveritas.com>
2023-03-21 18:47:10 +0100

    WIP Import and split resource browser from ENMap-Box

---
+++ pyqgis_resource_browser/core/resource_table_model.py
@@ -0,0 +1,67 @@
+import os
+from typing import Any
+
+from qgis.PyQt.QtCore import QAbstractTableModel, QModelIndex, Qt
+from qgis.PyQt.QtGui import QIcon
+
+
+class ResourceTableModel(QAbstractTableModel):
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+
+        self.cnUri = "Path"
+        self.cnIcon = "Resource"
+        self.RESOURCES = []
+        self.reloadResources()
+
+    def reloadResources(self):
+        self.beginResetModel()
+        self.RESOURCES.clear()
+        self.RESOURCES.extend(list(scanResources()))
+        self.endResetModel()
+
+    def columnCount(self, parent: QModelIndex = ...) -> int:
+        return 2
+
+    def rowCount(self, parent: QModelIndex = ...) -> int:
+        return len(self.RESOURCES)
+
+    def columnNames(self) -> list[str]:
+        return [self.cnUri, self.cnIcon]
+
+    def headerData(
+        self, section: int, orientation: Qt.Orientation, role: int = ...
+    ) -> Any:
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return self.columnNames()[section]
+
+        if role == Qt.TextAlignmentRole and orientation == Qt.Vertical:
+            return Qt.AlignRight
+
+        return super().headerData(section, orientation, role)
+
+    def data(self, index: QModelIndex, role: int = ...) -> Any:
+        if not index.isValid():
+            return None
+
+        uri = self.RESOURCES[index.row()]
+        cn = self.columnNames()[index.column()]
+
+        if role == Qt.DisplayRole:
+            if cn == self.cnUri:
+                return uri
+            else:
+                return os.path.basename(uri)
+        if role == Qt.DecorationRole:
+            if cn == self.cnIcon:
+                return QIcon(uri)
+
+        if role == Qt.ToolTipRole:
+            if cn == self.cnUri:
+                return uri
+
+        if role == Qt.UserRole:
+            return uri
+
+        return None
---
+++ pyqgis_resource_browser/core/resource_table_view.py
@@ -0,0 +1,34 @@
+import os
+
+from qgis.PyQt.QtCore import QModelIndex, Qt
+from qgis.PyQt.QtGui import QContextMenuEvent, QPixmap
+from qgis.PyQt.QtWidgets import QApplication, QMenu, QTableView
+
+
+class ResourceTableView(QTableView):
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+
+    def contextMenuEvent(self, event: QContextMenuEvent) -> None:
+        idx = self.indexAt(event.pos())
+        if isinstance(idx, QModelIndex) and idx.isValid():
+            uri = idx.data(Qt.UserRole)
+            m = QMenu()
+            a = m.addAction("Copy Name")
+            a.triggered.connect(
+                lambda *args, n=os.path.basename(uri): QApplication.clipboard().setText(
+                    n
+                )
+            )
+            a = m.addAction("Copy Path")
+            a.triggered.connect(
+                lambda *args, n=uri: QApplication.clipboard().setText(n)
+            )
+            a = m.addAction("Copy Icon")
+            a.triggered.connect(
+                lambda *args, n=uri: QApplication.clipboard().setPixmap(QPixmap(n))
+            )
+
+            m.exec_(event.globalPos())
+
+        pass
