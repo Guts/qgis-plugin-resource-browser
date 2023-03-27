a888acc0abac19f537b9985997e174cce206635d
GeoJulien <dev@ingeoveritas.com>
2023-03-27 18:14:37 +0200

    Fix and improve some texts

---
+++ pyqgis_resource_browser/resources/i18n/pyqgis_resource_browser_fr.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>PlgPyQgisResourceBrowserPlugin</name>
+    <message>
+        <location filename="../../plugin_main.py" line="90"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../../plugin_main.py" line="74"/>
+        <source>Browse resources</source>
+        <translation>Parcourir les images</translation>
+    </message>
+    <message>
+        <location filename="../../plugin_main.py" line="81"/>
+        <source>Documentation</source>
+        <translation>Documentation</translation>
+    </message>
+</context>
+<context>
+    <name>wdg_resource_browser</name>
+    <message>
+        <location filename="../../gui/resource_browser.ui" line="14"/>
+        <source>PyQGIS Icons Browser</source>
+        <translation>Navigateur d'icônes de QGIS</translation>
+    </message>
+    <message>
+        <location filename="../../gui/resource_browser.ui" line="40"/>
+        <source>Filter</source>
+        <translation>Filtrer</translation>
+    </message>
+    <message>
+        <location filename="../../gui/resource_browser.ui" line="139"/>
+        <source>Image</source>
+        <translation>Image</translation>
+    </message>
+    <message>
+        <location filename="../../gui/resource_browser.ui" line="183"/>
+        <source>Text</source>
+        <translation>Textuel</translation>
+    </message>
+    <message>
+        <location filename="../../gui/resource_browser.ui" line="228"/>
+        <source>Activate to use regular expressions</source>
+        <translation>Active pour utiliser des expressions régulières</translation>
+    </message>
+    <message>
+        <location filename="../../gui/resource_browser.ui" line="240"/>
+        <source>Reload</source>
+        <translation>Recharger</translation>
+    </message>
+</context>
+<context>
+    <name>wdg_resource_browser_settings</name>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="14"/>
+        <source>PyQGIS Resource Browser - Settings</source>
+        <translation>Navigateur de ressources de QGIS - Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="72"/>
+        <source>Filters</source>
+        <translation>Filtres</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="78"/>
+        <source>Activate to show resources from specific domains only.</source>
+        <translation>Activer pour ne voir que les ressources de certains domaines.</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="81"/>
+        <source>Resource Prefix</source>
+        <translation>Préfixe des ressources</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="124"/>
+        <source>Activate to show resources with specfic file type only.</source>
+        <translation>Activer pour ne voir que les ressources des types de fichiers listés.</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="127"/>
+        <source>File Types</source>
+        <translation>Types de fichiers</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="161"/>
+        <source>Miscellaneous</source>
+        <translation>Divers</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="285"/>
+        <source>Report an issue</source>
+        <translation>Signaler une anomalie</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="210"/>
+        <source>Version used to save settings:</source>
+        <translation>Version utilisée pour sauvegarder ces paramètres :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="307"/>
+        <source>Help</source>
+        <translation>Documentation</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="232"/>
+        <source>Reset setttings to factory defaults</source>
+        <translation>Réinitialiser les paramètres aux valeurs par défaut</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="251"/>
+        <source>Enable debug mode.</source>
+        <translation>Activer le mode debug.</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="260"/>
+        <source>Debug mode (degraded performances)</source>
+        <translation>Mode debug (performances dégradées)</translation>
+    </message>
+</context>
+</TS>
